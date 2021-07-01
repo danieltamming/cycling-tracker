28697a939ef1baaf2c3f76f0292ae918a85b0658
Daniel Tamming <daniel.tamming@unityhealth.to>
2021-07-01 11:03:52 -0400

    Updating all figures at once

--- plotly_live.py
+++ plotly_live.py
@@ -25,7 +25,7 @@ cap = cv2.VideoCapture('../cycling-tracker-private/videos/short_vid3.MOV')
 #     q = pickle.load(f)
 
 frame_list = []
-for _ in range(10*30):
+for _ in range(2*30):
     ret, frame = cap.read()
     if not ret:
         break
@@ -45,39 +45,31 @@ app.layout = html.Div(
         ),
         dcc.Interval(
             id='interval-component',
-            interval=1/10*1000, # in milliseconds
+            interval=2*1000, # in milliseconds
             n_intervals=0
-        ),
-        dcc.Store(id='intermediate-value')
+        )
     ])
 )
 
+
 @app.callback(
-    Output('intermediate-value', 'data'),
-    Input('interval-component', 'n_intervals'))
-def clean_data(n):
-    return n
-
-
-@app.callback(Output('live-update-text', 'children'),
-              Input('intermediate-value', 'data'))
-def update_metrics(n):
-    print(n)
-    # lon, lat, alt = satellite.get_lonlatalt(datetime.datetime.now())
+    [
+        Output('live-update-text', 'children'),
+        Output('image', 'children'),
+        Output('speed-graph', 'figure')
+    ],
+    Input('interval-component', 'n_intervals')
+)
+def update_figures(n):
     frame_count, second, rpm, speed, distance = df.iloc[n]
     style = {'padding': '5px', 'fontSize': '16px'}
-    return [
+    text = [
         html.Span('Frame: {0:.2f}'.format(frame_count), style=style),
         html.Span('Second: {0:.2f}'.format(second), style=style),
         html.Span('RPM: {0:0.2f}'.format(rpm), style=style)
     ]
 
-
-# Multiple components can update everytime interval gets fired.
-@app.callback(Output('speed-graph', 'figure'),
-              Input('intermediate-value', 'data'))
-def update_graph_live(n):
-    n = int(n / 10)
+    # n = int(n / 10)
     fig = plotly.tools.make_subplots(rows=1, cols=1, vertical_spacing=0.2)
     y_min = min(20, df['speed'].iloc[:n+1].min())
     y_max = max(30, df['speed'].iloc[:n+1].max())
@@ -89,17 +81,7 @@ def update_graph_live(n):
         'mode': 'lines+markers',
         'type': 'scatter'
     }, row=1, col=1)
-    return fig
 
-# Multiple components can update everytime interval gets fired.
-@app.callback(Output('image', 'children'),
-              Input('intermediate-value', 'data'))
-def update_graph_live(n):
-    # for _ in range(1):
-    #     ret, frame = cap.read()
-    # ret, frame = cap.read()
-    # if not ret:
-    #     return dash.no_update
     frame = frame_list[3*n]
     k = 0.4
     new_width = int(k * frame.shape[1])
@@ -107,7 +89,46 @@ def update_graph_live(n):
     frame = cv2.resize(frame, (new_width, new_height), interpolation=cv2.INTER_AREA)
     retval, my_buffer = cv2.imencode('.jpg', frame)
     encoded_image = base64.b64encode(my_buffer)
-    return [html.Img(src='data:image/png;base64,{}'.format(encoded_image.decode()))]
+    image = [html.Img(src='data:image/png;base64,{}'.format(encoded_image.decode()))]
+
+    return text, image, fig
+
+
+# # Multiple components can update everytime interval gets fired.
+# @app.callback(Output('speed-graph', 'figure'),
+#               Input('intermediate-value', 'data'))
+# def update_graph_live(n):
+#     n = int(n / 10)
+#     fig = plotly.tools.make_subplots(rows=1, cols=1, vertical_spacing=0.2)
+#     y_min = min(20, df['speed'].iloc[:n+1].min())
+#     y_max = max(30, df['speed'].iloc[:n+1].max())
+#     fig.update_layout(yaxis_range=[y_min, y_max])
+#     fig.append_trace({
+#         'x': df['second'].iloc[:n+1],
+#         'y': df['speed'].iloc[:n+1],
+#         'name': 'Altitude',
+#         'mode': 'lines+markers',
+#         'type': 'scatter'
+#     }, row=1, col=1)
+#     return fig
+
+# # Multiple components can update everytime interval gets fired.
+# @app.callback(Output('image', 'children'),
+#               Input('intermediate-value', 'data'))
+# def update_graph_live(n):
+#     # for _ in range(1):
+#     #     ret, frame = cap.read()
+#     # ret, frame = cap.read()
+#     # if not ret:
+#     #     return dash.no_update
+#     frame = frame_list[3*n]
+#     k = 0.4
+#     new_width = int(k * frame.shape[1])
+#     new_height = int(k * frame.shape[0])
+#     frame = cv2.resize(frame, (new_width, new_height), interpolation=cv2.INTER_AREA)
+#     retval, my_buffer = cv2.imencode('.jpg', frame)
+#     encoded_image = base64.b64encode(my_buffer)
+#     return [html.Img(src='data:image/png;base64,{}'.format(encoded_image.decode()))]
 
 def open_webpage():
     webbrowser.open_new('http://localhost:8050/')
